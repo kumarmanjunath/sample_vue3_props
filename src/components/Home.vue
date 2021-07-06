eff976f7d317130b4bf72856faab7e9bfcbefc53
kumarmanjunath <kumarmanjunath197@gmail.com>
2021-07-06 10:17:43 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <h1>Home Sweet Home</h1>
+        <PostList :posts="posts"/>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import PostList from './PostList.vue'
+export default {
+    name:'Home',
+    components:{PostList},
+    setup () {
+        const posts=ref([
+      {title:'welcome to the blog', body:'Loremd fff',id:1},
+       {title:'top 5 css tips', body:'Loremd ddd',id:2}
+    ])
+        
+
+        return {posts}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+<h1>
+    {{post.title}}
+</h1>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props:['posts']
+
+}
+</script>
+
+<style>
+
+</style>
